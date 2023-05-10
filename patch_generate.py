9b37a8c3c27a415770d4f0ec3309b03c7344f51e
jeromefan <fanfannn98@gmail.com>
2023-05-10 11:50:16 +0800

    update

--- patch_generate.py
+++ patch_generate.py
@@ -7,32 +7,35 @@ import argparse
 from tqdm import tqdm
 from PIL import Image
 from pathlib import Path
-from copy import deepcopy
 from torchvision import transforms
-from engine.misc import number_classes, class_names
+from engine.misc import square_transform
+from torch.autograd import Variable
+import torch.nn.functional as F
 
 
 def get_args_parser():
 
     parser = argparse.ArgumentParser('以Yolo v5为攻击模型, 生成有效的攻击',
                                      add_help=False)
-    parser.add_argument('--image_size', default=640, type=int,
+    parser.add_argument('--img-size', default=640, type=int,
                         help='图像缩放大小')
+    parser.add_argument('--cls', default=1, type=int,
+                        help='攻击目标类别')
+    parser.add_argument('--max-iter', default=1000, type=int,
+                        help='攻击目标类别')
     parser.add_argument('--hyp', default='engine/hyp.scratch.yaml', type=str,
                         help='hyperparameters path')
 
     return parser
 
 
-def build_targets(detect_model, data, target_label):
-    detections = detect_model(data)
-    annotated_img = detections.render()[0]
-    annotated_img = Image.fromarray(
-        annotated_img.astype('uint8')).convert('RGB')
-    annotated_img.save('results/an.png')
-    xywhn = detections.xywhn[0]
-    targets = [0.0, target_label, xywhn[0, 0].item(
-    ), xywhn[0, 1].item(), xywhn[0, 2].item(), xywhn[0, 3].item()]
+def build_targets(model, data, target_label):
+    model.eval()
+    pred = non_max_suppression(model(data))
+    xyxy = pred[0][0, :4]
+    xywhn = xyxy2xywhn(xyxy.unsqueeze(0)).squeeze(0)
+    targets = [0.0, target_label, xywhn[0].item(), xywhn[1].item(),
+               xywhn[2].item(), xywhn[3].item()]
     return torch.tensor(targets).unsqueeze(0)
 
 
@@ -44,107 +47,96 @@ def main(args):
     # model
     repo_loc = Path(os.path.dirname(
         os.path.abspath(__file__))+'/engine/yolov5/')
-    detect_model = torch.hub.load(repo_or_dir=repo_loc,
-                                  model='custom',
-                                  path=Path('./weights/yolov5s.pt'),
-                                  source='local',
-                                  device=0 if torch.cuda.is_available() else 'cpu')
-    patch_trans_1 = transforms.Compose([
-        transforms.Resize((60, 120)),
-        transforms.ToTensor()
-    ])
-
-    patch_trans_2 = transforms.Compose([
-        transforms.Resize((120, 60)),
-        transforms.ToTensor()
-    ])
-
-    patch_trans_3 = transforms.Compose([
-        transforms.Resize((120, 120)),
-        transforms.ToTensor()
-    ])
+    model = torch.hub.load(repo_or_dir=repo_loc,
+                           model='custom',
+                           path=Path('./weights/yolov5s.pt'),
+                           source='local',
+                           autoshape=False,
+                           device=0 if torch.cuda.is_available() else 'cpu')
+    compute_loss = ComputeLoss(model.model)
 
     trans = transforms.Compose([
-        transforms.Resize((640, 640)),
+        transforms.Resize((args.img_size, args.img_size)),
         transforms.ToTensor()
     ])
-
     unloader = transforms.ToPILImage()
 
     # data
-    patch_param_1 = patch_trans_1(Image.open(
-        'assets/zidane.jpg')).unsqueeze(0).to(device)
-    patch_param_2 = patch_trans_2(Image.open(
-        'assets/zidane.jpg')).unsqueeze(0).to(device)
-    patch_param_3 = patch_trans_3(Image.open(
-        'assets/zidane.jpg')).unsqueeze(0).to(device)
-    data = trans(Image.open('assets/T_stop_d.TGA'))
-    targets = build_targets(detect_model, unloader(data), 40)
+    data = trans(Image.open('assets/T_stop_d.TGA')).unsqueeze(0).to(device)
+    targets = build_targets(model, data, args.cls)
+    patch_transformed, mask = square_transform(
+        data.shape, args.img_size)
+    targets, patch_transformed, mask = targets.to(device), patch_transformed.to(
+        device), mask.to(device)
+    data_with_patch = torch.mul(
+        (1 - mask), data) + torch.mul(mask, patch_transformed)
+    print(f'Target is: {targets}')
 
-    data = data.unsqueeze(0).to(device)
-    targets = targets.to(device)
-    print(targets)
-    del detect_model
+    model.eval()
+    target_pred = (non_max_suppression(
+        model(data_with_patch))[0].cpu()[0, 5]).item()
 
-    ckpt = torch.load('weights/yolov5s.pt', map_location='cpu')
-    model = Model(ckpt['model'].yaml, ch=3, nc=80).to(device)
+    save_name = None
+    iterator = tqdm(range(args.max_iter))
+    for count in iterator:
 
-    with open(args.hyp) as f:
-        hyp = yaml.safe_load(f)
-    nl = model.model[-1].nl
-    hyp['box'] *= 3. / nl
-    hyp['cls'] *= number_classes / 80. * 3. / nl
-    hyp['obj'] *= (args.image_size / 640) ** 2 * 3. / nl
-    model.nc = number_classes
-    model.hyp = hyp
-    model.gr = 1.0
-    model.names = class_names
+        if target_pred == args.cls:
+            save_name = f'results/{count}.png'
+            unloader(data_with_patch.squeeze(0)).save(save_name)
+            break
 
-    loss_calculator = ComputeLoss(model)
+        model.train()
+        count += 1
 
-    for epoch in tqdm(range(500)):
+        data_with_patch = Variable(
+            data_with_patch.data, requires_grad=True)
+        data_with_patch = data_with_patch.to(device)
+        target_loss, _ = compute_loss(model(data_with_patch), targets)
+        target_loss.backward()
 
-        for i in range(450, 570):
-            for j in range(350, 410):
-                data[0, 0, j, i] = patch_param_1[0, 0, j-350, i-450]
-                data[0, 1, j, i] = patch_param_1[0, 1, j-350, i-450]
-                data[0, 2, j, i] = patch_param_1[0, 2, j-350, i-450]
-        for i in range(190, 250):
-            for j in range(360, 480):
-                data[0, 0, j, i] = patch_param_2[0, 0, j-360, i-190]
-                data[0, 1, j, i] = patch_param_2[0, 1, j-360, i-190]
-                data[0, 2, j, i] = patch_param_2[0, 2, j-360, i-190]
-        for i in range(280, 400):
-            for j in range(300, 420):
-                data[0, 0, j, i] = patch_param_3[0, 0, j-300, i-280]
-                data[0, 1, j, i] = patch_param_3[0, 1, j-300, i-280]
-                data[0, 2, j, i] = patch_param_3[0, 2, j-300, i-280]
+        patch_grad = data_with_patch.grad.data.clone()
+        data_with_patch.grad.data.zero_()
+        patch_transformed += 0.5 * patch_grad
+        patch_transformed = torch.clamp(patch_transformed, min=0., max=1.)
 
-        x_adv = data.detach().clone()
-        x_adv.requires_grad = True
-        y = model(x_adv)
-        loss, _ = loss_calculator(y, targets)
+        # Test
+        data_with_patch = torch.mul(
+            (1-mask), data) + torch.mul(mask, patch_transformed)
+        data_with_patch = data_with_patch.to(device)
+        model.eval()
 
-        grad = torch.autograd.grad(
-            outputs=loss, inputs=x_adv)[0].sign()
+        target_pred = non_max_suppression(model(data_with_patch))[0].cpu()
+        if target_pred.shape != torch.Size([0, 6]):
+            target_pred = (target_pred[0, 5]).item()
+        else:
+            target_pred = 11
+    del model
+    return save_name
 
-        patch_param_1 += 0.001 * grad[:, :, 350:410, 450:570]
-        patch_param_2 += 0.001 * grad[:, :, 360:480, 190:250]
-        patch_param_3 += 0.001 * grad[:, :, 300:420, 280:400]
 
-        if (epoch+1) in [5, 100, 500]:
-
-            img_new = unloader(
-                deepcopy(data.detach().cpu().squeeze(0))
-            )
-            img_new.save(f'results/triple_{epoch+1}.png')
+def test(save_name):
+    if save_name is not None:
+        model = torch.hub.load(repo_or_dir='/home/ubuntu/workspace/StopSignDET/engine/yolov5/',
+                               model='custom',
+                               path=Path('./weights/yolov5s.pt'),
+                               source='local',
+                               device='cpu')
+        img = Image.open(save_name)
+        annotated_img = Image.fromarray(
+            model(img).render()[0].astype('uint8')).convert('RGB')
+        annotated_img.save('results/render.png')
+    else:
+        print('Attack Fail!')
 
 
 if __name__ == '__main__':
     sys.path.append('engine/yolov5')
     from engine.yolov5.utils.loss import ComputeLoss
-    from engine.yolov5.models.yolo import Model
+    from engine.yolov5.utils.general import non_max_suppression, xyxy2xywhn
+
+    torch.set_printoptions(precision=4, sci_mode=False)
 
     args = get_args_parser()
     args = args.parse_args()
-    main(args)
+    save_name = main(args)
+    test(save_name)
